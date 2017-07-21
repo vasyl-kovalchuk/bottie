49ba44ce04ccf53e4b8f60038f9e9d64cc298362
vasyl-kovalchuk <vasyl.kovalchuk@logivations.com>
2017-07-21 14:59:12 +0300

    added Create Presentation skill

---
+++ skills/CreatePR.js
@@ -0,0 +1,7 @@
+
+module.exports = function(skill, info, bot, message, services) {
+  console.log('INVOCATION OF NON-CONFIGURED SKILL: ' + skill);
+
+  bot.reply(message, 'I understood this as `' + skill +
+    '`, but you haven\'t configured how to make me work yet!');
+};
