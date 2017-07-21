d9d7a939ef3ccb43bc1bcc8b81dbae53ffbaf674
VolodyaKhoma <volodymyr.khoma@logivations.com>
2017-07-21 17:30:33 +0300

    slides service improve

--- src/services/SlidesService.js
+++ src/services/SlidesService.js
@@ -20,72 +20,60 @@ class SlidesService {
 
     //version.name , version.releasedDate
     writeSlidesTemplate(version, issues){
-        return new Promise(function (resolve,reject) {
-            fs.writeFile(`res/slides.md`, "" +
-                "---\n" +
-                "# This is a title slide\n" +
-                "##Version " + version.name +"\n"+
-                "---\n"+
-                "---\n" +
-                "![](https://image.ibb.co/cf2S75/bg.png){.background}\n" +
-                "# :heart_eyes_cat:                                        Demo Feature Meeting\n" +
-                "##<span>Version 7.12                                                          Date 21.07.17                                                        </span>\n" +
-                "\n" +
-                "---\n" +
+
+
+        let issueString = "";
+        let issueSlides = "";
+        let i = 1;
+        issues.forEach(issue => {
+            issueString += "\n" + i + ". " + issue.summary + " (" + issue.id + ") " + " assignee - " + issue.developer;
+            issueSlides += "\n---\n ![](https://image.ibb.co/cf2S75/bg.png){.background}\n #<span style=\"color:whitesmoke\"> </span>                                                    \n<span>                                                       </span><span style=\"text-decoration:underline;color:blue\">WMO-"+issue.id+"</span>                  \n"+
+                "<span><b>        "+issue.summary+"</b> </span>                     \n"+
+                "<span>                                        assignee: "+issue.developer+"</span>\n             "
+            i++;
+        });
+
+        return new Promise(function (resolve, reject) {
+            fs.writeFile('res/slides.md', "---\n![](https://image.ibb.co/cf2S75/bg.png){.background}\n# :heart_eyes_cat:                                        Demo Feature Meeting\n##<span>Version " + version.name + "                                                          Date " + version.releasedDate + "                                                        </span>" +
+                "\n---\n" +
                 "![](https://image.ibb.co/cFsnqQ/bg.png){.background}\n" +
-                "#<span style=\"color:green\">                    Demo Feature Meeting v7.10</span>\n" +
+                "#<span style=\"color:green\">                    Demo Feature Meeting v." + version.name + "</span>\n" +
                 "<span>                                                                Agenda                         \n" +
                 "                                            <b>Development Teams:</b></span> \n" +
                 "</span>\n" +
-                "\n" +
-                "1. LNO: Create option in UI to export all polygons (WMO-57611) by Volodymyr Khoma\n" +
-                "2. Feature 2\n" +
-                "3. Feature 3\n" +
-                "\n" +
-                "<b>Project Team:</b>\n" +
+                issueString +
+                "\n\n<b>Project Team:</b>\n" +
                 "\n" +
                 "<b>Sales, Partner and Marketing News:</b>\n" +
-                "\n" +
-                "\n" +
-                "---\n" +
-                "![](https://image.ibb.co/jAbVAQ/devTitle.png){.background}\n" +
-                "# <span style=\"color:whitesmoke\"> </span>\n" +
-                "\n" +
-                "---\n" +
-                "![](https://image.ibb.co/cf2S75/bg.png){.background}\n" +
-                "# <span style=\"color:whitesmoke\"> </span>                                                    \n" +
-                "<span>                                                       </span><span style=\"text-decoration:underline;color:blue\">WMO-57611</span>           \n" +
-                "                   <span>                          <b>LNO: Create option in UI to export all polygons</b> </span>                  \n" +
-                "                   <span>                                                   by Volodymyr Khoma</span>\n" +
-                "\n" +
-                "---\n" +
-                "\n" +
+                "\n---" +
+                "\n![](https://image.ibb.co/jAbVAQ/devTitle.png){.background}" +
+                "\n# <span style=\"color:whitesmoke\"> </span>" +
+                "\n"+issueSlides +
+                "\n---\n" +
                 "![](https://image.ibb.co/jEeYVQ/project_News.png){.background}\n" +
-                "# <span style=\"color:whitesmoke\"> </span>\n" +
-                "\n" +
-                "---\n" +
-                "\n" +
-                "![](https://image.ibb.co/dayGjk/sales_News.png){.background}\n" +
-                "# <span style=\"color:whitesmoke\"> </span>\n" +
-                "\n" +
-                "---\n" +
-                "\n" +
-                "![](https://image.ibb.co/iuLyVQ/latest_Deals.png){.background}"
+                "# <span style=\"color:whitesmoke\"> </span>" +
+                "\n---\n" +
+                "\n![](https://image.ibb.co/dayGjk/sales_News.png){.background}" +
+                "\n# <span style=\"color:whitesmoke\"> </span>\n" +
+                "\n---\n" +
+                "\n![](https://image.ibb.co/iuLyVQ/latest_Deals.png){.background}"
 
-                , (err)=> {
+
+                , (err) => {
                     if (err) {
-                        console.error('writeFileError'+err);
+                        console.error('writeFileError' + err);
                         reject(err);
                     }
 
-                    this.createPresentation(resolve,reject);
+                    this.createPresentation(resolve, reject);
 
                 });
 
         });
+
     }
 
-    createPresentation(resolve,reject){
+    createPresentation(resolve, reject) {
         exec('md2gslides res/slides.md', (error, stdout, stderr) => {
 
             if (error) {
@@ -94,16 +82,17 @@ class SlidesService {
 
             }
 
-            let result = stdout.replace("Opening", "Here is");
-            result = result.replace("(", "");
-            result = result.replace(")", "");
+            let link = stdout.replace("Opening your presentation (", "");
+            link = link.replace(")", "");
             console.log(`-------------------------------------------stdout: ${stdout}`);
             console.log(`-------------------------------------------stderr: ${stderr}`);
 
-            resolve(result)
+            resolve(link)
         });
 
     }
+
+
 
 }
 
