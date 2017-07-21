c85254d477be84c7f58e5cb16a598b0cd10ccca4
VladyslavMelnyk <vladyslav.melnyk@logivations.com>
2017-07-21 16:23:51 +0300

    skills

--- skills/sharePR.js
+++ skills/sharePR.js
@@ -7,32 +7,30 @@ module.exports = function(skill, info, bot, message, db) {
     console.log('messaage text: ' + message.text);
     console.log('userdata' + userData);
   if (userData) {
-    // if there is a user named send ze hugs!
     for (var i = 0, len = userData.length; i < len; i++) {
-        HugUser(userData[i].replace(/[^\w\s]/gi, ''));
+        PRUser(userData[i].replace(/[^\w\s]/gi, ''));
         //console.log('userdate[i]' + userData[i]);
         //console.log('userdate[i]' + userData[i].replace(/[^\w\s]/gi, ''));
     }  
     //console.log(userData);
     bot.reply(message,'a presentation has been sent! :sunglasses:');
   } else {
-    bot.reply(message, 'You didn\'t name anyone that needed a DMF presentation ' + name + '?');
+    bot.reply(message, 'You didn\'t name anyone that needed a DFM presentation ' + name + '?');
   }
 
-function HugUser(user) {
-    //DM some love!
+function PRUser(user) {
     bot.api.im.open({ user: user }, function (err, response) {
       if (err) {
         return console.log(err)
       }
-    var hugArray = [
+    var prArray = [
     'https://giphy.com/gifs/eXTue7sCt6ZvG'
     ];
-    var randomHug = Math.floor(Math.random()* hugArray.length);    
+    var randomPR = Math.floor(Math.random()* prArray.length);    
 
     var dmChannel = response.channel.id
-    bot.say({channel: dmChannel, text: 'Someone sent you a DMF presentation!'})
-    bot.say({channel: dmChannel, text: hugArray[randomHug]})
+    bot.say({channel: dmChannel, text: 'Someone sent you a DFM presentation!'})
+    bot.say({channel: dmChannel, text: prArray[randomPR]})
     })    
     
     
